1a471a73cb85c5f9aab9580c5b741b5931685695
Ujjwal Paudel <upoudel888@gmail.com>
2023-12-06 15:35:47 +0545

    this must work

--- EZcrossword/settings.py
+++ EZcrossword/settings.py
@@ -120,15 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+if(DEBUG):
+    STATIC_URL = 'static/'
+else:
+    STATIC_URL = 'staticfiles/'
+    
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-if os.environ.get("VERCEL"):
-    STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 
 # Default primary key field type
--- EZcrossword/urls.py
+++ EZcrossword/urls.py
@@ -26,8 +26,8 @@ urlpatterns = [
     path("generator/",include("Generator.urls")),
 ]
 
-
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 
