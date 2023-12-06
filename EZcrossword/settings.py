a7b45ad200492cb47b76009bff2196ff001822be
Ujjwal Paudel <upoudel888@gmail.com>
2023-12-06 15:56:06 +0545

    static root changed

--- EZcrossword/settings.py
+++ EZcrossword/settings.py
@@ -123,9 +123,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
-
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles","static")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
